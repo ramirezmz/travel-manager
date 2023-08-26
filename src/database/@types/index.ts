a29da81344006976292710c92b3947264acdae1d
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-08-26 20:10:11 -0300

    feat: add params to read method iCrud

--- src/database/@types/index.ts
+++ src/database/@types/index.ts
@@ -2,7 +2,7 @@ export interface ICrud {
   isConnected(): Promise<any>;
   connect(): Promise<void>;
   create(item: any): Promise<any>;
-  read(item: any): Promise<any>;
+  read(item: any, skip: number, limit: number): Promise<any>;
   update(id: string, item: any): Promise<any>;
   delete(id: string): Promise<any>;
 }
