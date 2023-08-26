33dc097fbd4498b366376dbe1de9c16fa65d01f5
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-08-26 20:10:51 -0300

    feat: add correct params to read method ContextStrategy

--- src/database/context/ContextStragegy.ts
+++ src/database/context/ContextStragegy.ts
@@ -5,6 +5,9 @@ class ContextStrategy implements ICrud {
   constructor(database: ICrud) {
     this.database = database;
   }
+  read(item: any, skip: number, limit: number): Promise<any> {
+    return this.database.read(item, skip, limit);
+  }
   isConnected(): Promise<boolean> {
     return this.database.isConnected();
   }
@@ -13,9 +16,6 @@ class ContextStrategy implements ICrud {
   }
   create(item: any): Promise<any> {
     return this.database.create(item);
-  }
-  read(item: any): Promise<any> {
-    return this.database.read(item);
   }
   update(id: string, item: any): Promise<any> {
     return this.database.update(id, item);
