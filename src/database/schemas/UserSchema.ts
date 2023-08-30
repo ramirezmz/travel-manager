44ba309c34094b67de0c49f690cd15f41dafdd4c
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-08-29 23:13:36 -0300

    feat: add unique variable in email

--- src/database/schemas/UserSchema.ts
+++ src/database/schemas/UserSchema.ts
@@ -9,9 +9,20 @@ const UserSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   createdAt: {
     type: Date,
     default: new Date(),
+  },
+  createdBy: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
 });
 
