9ec79cf7789f9c44cae0db0d05df833bf650a11a
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-09-15 01:54:23 -0300

    🚨 feat: apply linter in project

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,9 @@
 import assert from "assert";
 
 describe("First test", function () {
-  describe("First", () => {
-    it("Should return correct", async () => {
-      assert.equal(1, 1);
-    });
-  });
+	describe("First", () => {
+		it("Should return correct", async () => {
+			assert.equal(1, 1);
+		});
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes';
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(router)
+app.use(router);
 app.listen(PORT, async () => {
-    console.log(`Server is running on port ${BASE_URL}🚀`);
-})
+	console.log(`Server is running on port ${BASE_URL}🚀`);
+});
